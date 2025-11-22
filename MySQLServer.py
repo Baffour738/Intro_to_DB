6e96defeb4fb6dd979840d730c297a808eb12f69
Akowuah Addo Baffour <akowuahab@gmail.com>
2025-11-22 18:31:23 +0000

    Create MySQLServer.py to manage database connection

---
+++ MySQLServer.py
@@ -0,0 +1,26 @@
+import mysql.connector
+from mysql.connector import Error
+
+def main():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=""           # change if needed
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        if connection and connection.is_connected():
+            connection.close()
+
+if __name__ == "__main__":
+    main()
